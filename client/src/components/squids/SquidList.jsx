984432c788dbf22980664abfa9f5e642a7b8c15a
nwalberts <nick.alberts@launchacademy.com>
2022-03-07 18:07:01 -0500

    add css edits without console

--- client/src/components/squids/SquidList.jsx
+++ client/src/components/squids/SquidList.jsx
@@ -6,21 +6,21 @@ import { SquidListTile } from "./SquidListTile";
 import "../../style/squids/squidList.pcss";
 
 export const SquidList = () => {
-  const squidListQuery = useSquidList();
-  const squids = squidListQuery.data || [];
+  const { data, isLoading, isFetching, isError, error } = useSquidList();
+  const squids = data || [];
 
   const squidTiles = squids.map((squid) => <SquidListTile key={squid.id} {...squid} />);
 
   // eslint-disable-next-line no-nested-ternary
-  return squidListQuery.isLoading ? (
+  return isLoading ? (
     "Loading..."
-  ) : squidListQuery.isError ? (
-    squidListQuery.error.message
+  ) : isError ? (
+    error.message
   ) : (
-    <div className="squid-list">
-      {squidTiles}
+    <div className="squid-page">
+      <div className="squid-list">{squidTiles}</div>
 
-      {squidListQuery.isFetching ? "Updating..." : null}
+      {isFetching ? "Updating..." : null}
     </div>
   );
 };
